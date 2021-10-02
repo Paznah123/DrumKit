e43a6177396dad1839867e1ed4496c3bd16bce8d
Paznah123 <61472807+Paznah123@users.noreply.github.com>
2021-10-02 15:18:28 +0300

    finished website

---
+++ index.js
@@ -0,0 +1,42 @@
+var btns = document.querySelectorAll(".drum");
+
+const sounds = {
+    "w": "crash",
+    "a": "kick-bass",
+    "s": "snare",
+    "d": "tom-1",
+    "j": "tom-2",
+    "k": "tom-3",
+    "l": "tom-4"
+};
+
+// button click events
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var btn_class = this.getAttribute("class")[0];
+        var sound_name = sounds[btn_class];
+        playSound(sound_name);
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+// keypress events
+document.addEventListener('keypress', (e) => {
+    var sound_name = sounds[e.key];
+    playSound(sound_name);
+    buttonAnimation(e.key);
+});
+
+function playSound(sound_name) {
+    var audio = new Audio("sounds/" + sound_name + ".mp3");
+    audio.play();
+}
+
+function buttonAnimation(btn_name) {
+    var active_btn = document.querySelector("." + btn_name);
+    active_btn.classList.add("pressed");
+
+    setTimeout(function () {
+        active_btn.classList.remove("pressed");
+    }, 100)
+}
